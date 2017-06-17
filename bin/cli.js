5b27a284ab02573355394d7153f056138bc660b3
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2017-06-18 00:29:35 +0200

    v1.3.8 - Bump SC version

--- bin/cli.js
+++ bin/cli.js
@@ -314,7 +314,7 @@ if (command == 'create') {
   } catch (e) {}
 
   var dockerCommand = `docker run -d -p ${portNumber}:8000 -v ${absoluteAppPath}:/usr/src/app/ -e "SOCKETCLUSTER_WORKER_CONTROLLER=/usr/src/app/worker.js"` +
-    `${envFlagString} --name ${appName} socketcluster/socketcluster:v5.14.2`;
+    `${envFlagString} --name ${appName} socketcluster/socketcluster:v5.15.0`;
 
   try {
     execSync(dockerCommand, {stdio: 'inherit'});
