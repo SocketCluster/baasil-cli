844399db22a69af0f4932df876a96cb870fecebc
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2017-07-04 22:11:36 +0200

    v1.4.3

--- bin/cli.js
+++ bin/cli.js
@@ -314,7 +314,7 @@ if (command == 'create') {
   } catch (e) {}
 
   var dockerCommand = `docker run -d -p ${portNumber}:8000 -v ${absoluteAppPath}:/usr/src/app/ -e "SOCKETCLUSTER_WORKER_CONTROLLER=/usr/src/app/worker.js"` +
-    `${envFlagString} --name ${appName} socketcluster/socketcluster:v6.0.1`;
+    `${envFlagString} --name ${appName} socketcluster/socketcluster:v6.0.2`;
 
   try {
     execSync(dockerCommand, {stdio: 'inherit'});
